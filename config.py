c08a16405e61dc29a621218f0da7bbda11a0385a
Nahid270270 <muktaaktar772211@gmail.com>
2025-05-15 21:19:44 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "1234567"))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001234567890"))
-    DATABASE_URL = os.getenv("DATABASE_URL", "")
-    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", "5"))
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))
+    API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
+    API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-100XXXXXXXXXX"))  # যেখান থেকে মুভি মেসেজগুলো আসবে
+    DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://user:pass@cluster.mongodb.net/dbname")
+    BOT_OWNER = int(os.getenv("BOT_OWNER", "123456789"))  # এডমিনের টেলিগ্রাম ইউজার আইডি
+    RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))  # সার্চ রেজাল্টস কতগুলো দেখাবে
