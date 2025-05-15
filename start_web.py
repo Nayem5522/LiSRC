8a50fd0a7fa52957793780adad5aefa3fe824964
Nahid270270 <muktaaktar772211@gmail.com>
2025-05-15 23:02:57 +0600

    Delete start_web.py

--- start_web.py
+++
@@ -1,8 +0,0 @@
-import os
-from http.server import SimpleHTTPRequestHandler, HTTPServer
-
-def start_web():
-    port = int(os.environ.get("PORT", 8000))
-    server = HTTPServer(("0.0.0.0", port), SimpleHTTPRequestHandler)
-    print(f"Web server running on port {port}")
-    server.serve_forever()
