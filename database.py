4ba7513b83f91aa094eebd80c034207592f135c4
Nahid270270 <muktaaktar772211@gmail.com>
2025-05-15 22:13:06 +0600

    Update database.py

--- database.py
+++ database.py
@@ -2,21 +2,24 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from config import Config
 
 client = AsyncIOMotorClient(Config.DATABASE_URL)
-db = client["autobot_db"]
-collection = db["movies"]
+db = client.autobot
+collection = db.files
 
 async def add_to_db(message):
     title = message.text or message.caption
     if not title:
         return
+    clean_title = " ".join(title.lower().strip().split())
     data = {
-        "message_id": message.message_id,
-        "title": title.lower()
+        "message_id": message.id,
+        "title": clean_title
     }
-    if not await collection.find_one({"message_id": message.message_id}):
+    if not await collection.find_one({"message_id": message.id}):
         await collection.insert_one(data)
 
 async def search_from_db(query: str, limit: int = 5):
-    regex = {"$regex": query, "$options": "i"}
+    query = " ".join(query.lower().strip().split())
+    regex = {"$regex": f".*{query}.*", "$options": "i"}
     cursor = collection.find({"title": regex}).limit(limit)
-    return await cursor.to_list(length=limit)
+    results = await cursor.to_list(length=limit)
+    return results
