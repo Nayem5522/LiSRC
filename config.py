ca4d91d4449e189c14657962c9bba036a08f566f
Nahid270270 <muktaaktar772211@gmail.com>
2025-05-15 20:48:10 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(os.getenv("API_ID", 12345))
     API_HASH = os.getenv("API_HASH", "")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -100))  # Source Channel
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -100))  # Channel to Save From
     BOT_OWNER = int(os.getenv("BOT_OWNER", 12345678))
     DATABASE_URL = os.getenv("DATABASE_URL", "")
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
